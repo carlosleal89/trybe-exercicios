59c453f9b9c693862aec0124cc3c76c547c6b411
Carlos Leal <carlos.frleal@gmail.com>
2023-01-24 13:48:26 -0300

    laço for

---
+++ fundamentos/secao-03-introducao-a-javaScript/dia-2-array-e-loop-for/lacoFor.js
@@ -0,0 +1,14 @@
+// tabuada
+
+let numero = 8;
+
+for(let i=1;i<=10;i++){
+    console.log(numero + 'X' + i + '=' + numero * i);
+}
+
+let listaNomes = ['Carlos', 'Maria', 'Ordaqui'];
+
+for(let i=0;i<listaNomes.length;i++){
+    console.log("Seja bem vindo " + listaNomes[i]);
+}
+
