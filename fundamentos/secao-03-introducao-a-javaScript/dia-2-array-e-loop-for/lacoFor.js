cd2e8dcf91016040ca1b114d36b25f79a74d1dea
Carlos Leal <carlos.frleal@gmail.com>
2023-01-24 15:27:29 -0300

    laço for e for of

--- fundamentos/secao-03-introducao-a-javaScript/dia-2-array-e-loop-for/lacoFor.js
+++ fundamentos/secao-03-introducao-a-javaScript/dia-2-array-e-loop-for/lacoFor.js
@@ -12,3 +12,10 @@ for(let i=0;i<listaNomes.length;i++){
     console.log("Seja bem vindo " + listaNomes[i]);
 }
 
+//Utilize o for/of para imprimir os elementos da lista names com o console.log():
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let name of names){ //cria a variavel 'name' e cada vez que passa no laço ele atribui um elemento do array 'names'.
+    console.log(name); 
+} 
