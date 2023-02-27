06bc521d701bdb81f3aa6bf0c9292b8a457eb511
Carlos Leal <carlos.frleal@gmail.com>
2023-02-27 13:34:35 -0300

    tst

--- fundamentos/secao-06-JSES6eTestesUnitarios/dia-01-FluxoDeExcessaoEObjetos/script3.js
+++ fundamentos/secao-06-JSES6eTestesUnitarios/dia-01-FluxoDeExcessaoEObjetos/script3.js
@@ -112,7 +112,7 @@ const lesson1 = {
 
   console.log(lessonCounter(allLessons));
 
-  // Utilizando o objeto (allLesson), crie uma função que deva retornar um objeto que represente o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes.
+  // Utilizando o objeto (allLesson),  crie uma função que deva retornar um objeto que represente o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes.
 
   function getInfo(object, teacher) {
     const objKeys = Object.keys(object);
