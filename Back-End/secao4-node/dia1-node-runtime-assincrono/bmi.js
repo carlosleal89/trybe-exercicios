0b1d01980ec3ec7fa8d244a41ed29e157a468165
Carlos Leal <carlos.frleal@gmail.com>
2023-07-17 19:59:12 -0300

    exercicio 4 e 5

--- Back-End/secao4-node/dia1-node-runtime-assincrono/bmi.js
+++ Back-End/secao4-node/dia1-node-runtime-assincrono/bmi.js
@@ -1,8 +1,15 @@
 const readline = require('readline-sync');
 
-const imc = (peso, altura) =>  (peso / altura**2).toFixed(2);
+const imcCalc = (peso, altura) =>  (peso / altura**2).toFixed(2);
 
 const peso = readline.questionFloat('Qual seu peso? ');
 const altura = readline.questionFloat('Qual sua altura? ');
 
-console.log(imc(peso, altura));
+const imc = Number(imcCalc(peso, altura));
+
+if (imc < 18.5) console.log('magro');
+if (imc >= 18.5 && imc < 25) console.log('normal');
+if (imc >= 25 && imc < 30) console.log('sobrepeso');
+if (imc >= 30 && imc < 35) console.log('obesidade 1');
+if (imc >= 35 && imc < 40) console.log('obesidade 2');
+if (imc > 40) console.log('obesidade 3');
