1c4844c8d6ce3d7088870ca46ca8cb6e7eb731e7
Carlos Leal <carlos.frleal@gmail.com>
2023-07-19 18:48:36 -0300

    configuração do ambiente e primeiro teste

---
+++ Back-End/secao4-node/dia3-testesIntegração/cacau_trybe/tests/integration/chocolates.test.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('Usando o metodo GET em /chocolates', function() {
+    it('Retorna a lista de chocolates', async function() {
+        const output = [
+            { id: 1, name: 'Mint Intense', brandId: 1 },
+            { id: 2, name: 'White Coconut', brandId: 1 },
+            { id: 3, name: 'Mon Chéri', brandId: 2 },
+            { id: 4, name: 'Mounds', brandId: 3 },
+          ];
+        const response = await chai
+            .request(app)
+            .get('/chocolates');
+        expect(response.status).to.be.equals(200);
+        expect(response.body.chocolates).to.deep.equal(output);
+    });
+});
